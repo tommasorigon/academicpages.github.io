e1eb0d1a132294bdfbd6f64db3971e77486ddb5e
Tommaso Rigon <tommasorigon@gmail.com>
2017-09-29 18:25:50 +0200

    locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,19 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Paris, France",
+    48.864716, 2.349014
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Rome, Italy",
+    41.890251, 12.492373
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Milan, Italy",
+    45.495525,
+    9.250907
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ]
+    "Caglari, italy",
+    39.2149029,
+    9.1094988  ]
 ];
